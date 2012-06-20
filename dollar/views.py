c2d023b19712eb528f250467e3827f45148a8119
Stuart Powers <stuart.powers@gmail.com>
2012-06-20 11:51:27 -0400

    autocommit: Wed Jun 20 11:51:27 EDT 2012

--- dollar/views.py
+++ dollar/views.py
@@ -10,9 +10,11 @@ def subscribe(request):
     if request.method == 'POST':
         print(request.POST)
         #HOW DO WE RETURN json like: "{result:1}"
-        return HttpResponse('{result:1, email:%s' % request.POST['email'])
-        return True
-    if request.method == 'GET':
+        return HttpResponse('{result:1, email:%s}' % request.POST['email'])
+    if request.method == 'GET': # GET WORKS!!!  it onlykinda works... no response is sent back
+                                #but it alsod oesn't 500 error out
         print "WERE IN GETGETGET"
-        return HttpResponse('{result:1, email:%s' % request.GET['email'])
-        return True
+        return HttpResponse('<b>hi there</b>')
+        #return HttpResponse('{result:1, email:%s}' % request.GET['email'])
+
+
