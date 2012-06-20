5158a1a6e333ab335feff91ca91d5b66eb9556b2
Stuart Powers <stuart.powers@gmail.com>
2012-06-20 12:07:28 -0400

    autocommit: Wed Jun 20 12:07:28 EDT 2012

--- dollar/views.py
+++ dollar/views.py
@@ -16,10 +16,14 @@ def subscribe(request):
     if request.method == 'GET': # GET WORKS!!!  it onlykinda works... no response is sent back
                                 #but it alsod oesn't 500 error out
         print "WERE IN GETGETGET"
-        abc = HttpResponse('{"town":1}', "application/json")
+        email=request.GET['email']
+        new_subscriber = Subscriber(email=email)
+
+        abc = HttpResponse('{"status":"fuckyeah", "email":email}', "application/json")
         print type(abc)
         print dir(abc)
-        new_subscriber = Subscriber(email=request.GET['email'])
         new_subscriber.save()
         return abc
         #return HttpResponse('{result:1, email:%s}' % request.GET['email'], content_type="application/json")
+
+
