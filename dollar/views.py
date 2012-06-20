279e411b09d63ba21a6fc68638c14b003b38bddd
Stuart Powers <stuart.powers@gmail.com>
2012-06-20 11:49:27 -0400

    autocommit: Wed Jun 20 11:49:27 EDT 2012

--- dollar/views.py
+++ dollar/views.py
@@ -1,16 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render_to_response, render
-from django.template import RequestContext
 from dollar.models import Subscriber
 
 
 def home(request):
-    return render_to_response('home.html', context_instance=RequestContext(request))
+    return render_to_response('home.html')
 
 def subscribe(request):
     if request.method == 'POST':
         print(request.POST)
+        #HOW DO WE RETURN json like: "{result:1}"
+        return HttpResponse('{result:1, email:%s' % request.POST['email'])
         return True
     if request.method == 'GET':
         print "WERE IN GETGETGET"
-        print(request.GET)
+        return HttpResponse('{result:1, email:%s' % request.GET['email'])
         return True
