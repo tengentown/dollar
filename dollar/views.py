cb45b587ef95f9c00a3e18cd736fd6ca5b6f4965
Stuart Powers <stuart.powers@gmail.com>
2012-06-20 11:24:30 -0400

    autocommit: Wed Jun 20 11:24:30 EDT 2012

---
+++ dollar/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render_to_response, render
+from dollar.models import Subscriber
+
+
+def home(request):
+	render_to_response('home.html')
+
+def subscribe(request):
+	if request.method == 'POST':
+        print(request.POST)
