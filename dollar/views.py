a4663122d9b21a717bfc097b451b916054f9eed8
Stuart Powers <stuart.powers@gmail.com>
2012-06-20 11:27:27 -0400

    autocommit: Wed Jun 20 11:27:27 EDT 2012

--- dollar/views.py
+++ dollar/views.py
@@ -3,8 +3,8 @@ from dollar.models import Subscriber
 
 
 def home(request):
-	render_to_response('home.html')
+    render_to_response('home.html')
 
 def subscribe(request):
-	if request.method == 'POST':
+    if request.method == 'POST':
         print(request.POST)
