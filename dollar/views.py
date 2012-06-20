272fe816425afd75f366f2f124bc07c0d6434762
Stuart Powers <stuart.powers@gmail.com>
2012-06-20 11:43:27 -0400

    autocommit: Wed Jun 20 11:43:27 EDT 2012

--- dollar/views.py
+++ dollar/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render_to_response, render
+from django.template import RequestContext
 from dollar.models import Subscriber
 
 
 def home(request):
-    return render_to_response('home.html')
+    return render_to_response('home.html', context_instance=RequestContext(request))
 
 def subscribe(request):
     if request.method == 'POST':
